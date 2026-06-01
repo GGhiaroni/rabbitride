668db887f648dd3be7438f37965f55bdb16001eb
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-01 12:06:10 -0300

    refactor: Refatora o local do SecurityConfig

--- services/user-service/src/main/java/com/gghiaroni/rabbitride/userservice/config/SecurityConfig.java
+++ services/user-service/src/main/java/com/gghiaroni/rabbitride/userservice/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.gghiaroni.rabbitride.userservice.user.config;
+package com.gghiaroni.rabbitride.userservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
